b573ab44cb5ca9cadaf344cfc9117f82d40569e9
JuanValladares1998 <valladaresjj2014@gmail.com>
2023-05-14 22:07:53 -0500

    agregados iconos a los botones de login y logout

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
   return (
     <header className="navbar bg-base-100 shadow-md">
       <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <a className="btn btn-ghost normal-case text-xl">Login de Usuarios</a>
       </div>
       <nav className="flex-none">
         <ul className="menu menu-horizontal px-1 gap-2">
@@ -44,17 +44,51 @@ const NavBar = () => {
                     signOut({ redirect: true, callbackUrl: "/" });
                   }}
                 >
-                  <a>Sign Out</a>
+                  <a>
+                    Salir{" "}
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75"
+                      />
+                    </svg>
+                  </a>
                 </li>
               </ul>
             </li>
           ) : (
             <>
               <li>
-                <Link href={"/auth/login"}>Sign In</Link>
+                <Link href={"/auth/login"}>
+                  Ingresar{" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                    />
+                  </svg>
+                </Link>
               </li>
               <li>
-                <Link className="btn btn-accent" href={"/auth/register"}>Register</Link>
+                <Link className="btn btn-accent" href={"/auth/register"}>
+                  Register
+                </Link>
               </li>
             </>
           )}
